4abcce3396a372e257230269f17e4bfb355477ff
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2022-05-30 17:58:31 -0700

    Make options optional

--- src/public/tga.ts
+++ src/public/tga.ts
@@ -10,6 +10,6 @@ import { IDecodedTga, IDecodeTgaOptions } from '../shared/types.js';
 // This file is the entry point the the library, it wraps the implementation files using dynamic
 // imports so the bare minimum code is loaded when code splitting is enabled.
 
-export async function decodeTga(data: Readonly<Uint8Array>, options: IDecodeTgaOptions): Promise<IDecodedTga> {
+export async function decodeTga(data: Readonly<Uint8Array>, options?: IDecodeTgaOptions): Promise<IDecodedTga> {
   return (await import('../decode/tga_decoder.js')).decodeTga(data, options);
 }
--- typings/api.d.ts
+++ typings/api.d.ts
@@ -4,7 +4,7 @@
  * Released under MIT license. See LICENSE in the project root for details.
  */
 
-export function decodeTga(data: Readonly<Uint8Array>, options: IDecodeTgaOptions): Promise<IDecodedTga>;
+export function decodeTga(data: Readonly<Uint8Array>, options?: IDecodeTgaOptions): Promise<IDecodedTga>;
 
 export interface IDecodedTga {
   image: IImage32;
